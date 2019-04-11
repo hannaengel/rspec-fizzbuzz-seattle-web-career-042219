afd556ae999edf33ec149261f706d248182d671f
Hanna Engel <hengel@alumni.berklee.edu>
2019-04-11 00:43:21 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0 && int%5==0
-    puts "FizzBuzz"
+  FizzBuzz =  "FizzBuzz"
   elsif int%3==0
-    puts "Fizz"
+    FizzBuzz ="Fizz"
   elsif int%5==0
-    puts "Buzz"
+    FizzBuzz = "Buzz"
+  return FizzBuzz 
   end
 end
