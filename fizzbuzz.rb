5cf10e06da3f78e9a1a9eeb0777354f7e920ff2f
Hanna Engel <hengel@alumni.berklee.edu>
2019-04-11 00:53:00 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(n)
   if ((n % 3 == 0) && (n%5==0))
-  "FizzBuzz"
+    "FizzBuzz"
 elsif (n%3==0)
   "Fizz"
   elsif (n%5==0)
-  "Buzz"
+    "Buzz"
   end
 end
